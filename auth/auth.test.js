b4940eac1dc5e1adbd786301ee57d3a691edc7f9
DeeDowns <dee_downs@outlook.com>
2020-09-18 12:11:54 -0400

    server tests pass

---
+++ auth/auth.test.js
@@ -0,0 +1,11 @@
+const supertest = require('supertest')
+
+const server = require('../api/server')
+const db = require('../database/dbConfig')
+
+describe('auth-router', () => {
+    describe('post to /api/auth/register', () => {
+        it.todo('should')
+    })
+    
+})
---
+++ jokes/jokes.test.js
@@ -0,0 +1,11 @@
+const supertest = require('supertest')
+
+const server = require('../api/server')
+const db = require('../database/dbConfig')
+
+describe('jokes-router', () => {
+    describe('GET to /api/jokes', () => {
+        it.todo('should')
+    })
+    
+})
