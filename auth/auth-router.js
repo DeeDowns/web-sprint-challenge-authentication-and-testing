ff57410284d4059923c657931b9fb07038dede8c
DeeDowns <dee_downs@outlook.com>
2020-09-18 13:10:36 -0400

    testing complete

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,14 +3,13 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const Users = require('../users/usersModel')
 
-router.post('/register', (req, res) => {
+router.post('/register', validateUser, (req, res) => {
   const credentials = req.body 
   
   const rounds = process.env.BCRYPT_ROUNDS || 8
   const hash = bcrypt.hashSync(credentials.password, rounds)
   credentials.password = hash
 
-  if(isValid(credentials)) {
     Users.add(credentials)
     .then(user => {
       console.log(user)
@@ -21,14 +20,14 @@ router.post('/register', (req, res) => {
       console.log(err)
       res.status(500).json({ message: err.message })
     })
-  }
+ 
 });
 
 
 
-router.post('/login', (req, res) => {
+router.post('/login', validateUser, (req, res) => {
   const { username, password } = req.body
-  if(isValid(req.body)) {
+
     Users.findBy({ username: username })
     .then(([user]) => {
       console.log(user)
@@ -46,16 +45,18 @@ router.post('/login', (req, res) => {
       console.log(err)
      res.status(500).json({ message: err.message })
     })
-  }
+  
 });
 
-function isValid(user) {
-  return (req, res, next) => {
-    if(user.username && user.password && typeof user.password === 'string') {
-      next()
-    } else {
-      res.status(400).json({ message: 'please provide username and password; password should be alphanumeric' })
-    }
+function validateUser(req, res, next) {
+  const { username, password } = req.body
+
+  if(!req.body) {
+    res.status(400).json({ message: 'missing content'})
+  } else if(!username && !password) {
+    res.status(400).json({ message: 'need username and password' })
+  } else {
+    next()
   }
 }
 
